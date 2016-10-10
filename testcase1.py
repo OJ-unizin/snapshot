33d7efef897dbf8f6d835990de6635c256d0fdd6
OJ-unizin <oscar.juarez@unizin.org>
2016-10-10 11:39:03 -0500

    update testcase1.py

--- testcase1.py
+++ testcase1.py
@@ -19,85 +19,107 @@ class TestCase1(unittest.TestCase):
     def test_AssertTitle(self):
         #Validate that the page tiltle is correct
         self.assertEqual(driver.title, "Log In to Canvas")
+        time.sleep(5)
 
     def test_assertText_Email(self):
         #Validate that the "Email" label is present.
-        webElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath("//label[@for='pseudonym_session_unique_id']"))
+        test_assertText_Email = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath("//label[@for='pseudonym_session_unique_id']"))
         #print webElement.text
-        assert webElement.text == 'Email'
-
+        assert test_assertText_Email.text == 'Email'
+        print 'test_assertText_Email'
+        time.sleep(5)
 
     def test_assertText_Password(self):
         #Validate that the "Password" label is present.
-        webElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath("//label[@for='pseudonym_session_password']"))
+        test_assertText_Password = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath("//label[@for='pseudonym_session_password']"))
         #print webElement.text
-        assert webElement.text == 'Password'
+        assert test_assertText_Password.text == 'Password'
+        print 'test_assertText_Password'
+        time.sleep(5)
 
 ##Find and Validate Input Elements
     def test_FindElement_emailInput(self):
         #Validate that the Email element is present.
-        webElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("pseudonym_session_unique_id"))
+        test_FindElement_emailInput = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("pseudonym_session_unique_id"))
         ##webElement = driver.find_element_by_id("pseudonym_session_unique_id")
         #webElement = driver.find_element_by_xpath("//input[@name='q']")
         #webElement = driver.find_element_by_css_selector("input[name=q]")
         #webElement = driver.find_element_by_name("q")
         ##webElement = driver.find_element_by_class_name("wsite-search-input")
         #print webElement
-
+        print 'test_FindElement_emailInput'
+        time.sleep(5)
 
     def test_FindElement_passwordInput(self):
         #Validate that the password element is present.
-        webElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("pseudonym_session_password"))
-
+        test_FindElement_passwordInput = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id("pseudonym_session_password"))
+        print 'test_FindElement_passwordInput'
+        time.sleep(5)
 
 ##Find and Validate Canvas logo
     def test_FindElement_canvasLogo(self):
         #Validate that the canvas logo is present.
-        webElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_css_selector("svg"))
-
+        test_FindElement_canvasLogo = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_css_selector("svg"))
+        print 'test_FindElement_canvasLogo'
+        time.sleep(5)
 
 
 ##Find and Validate Button Elements
     def test_FindAnElementButton_LogIn(self):
         #Validate that the "Log In" button is present.
-        webElement_LogInbtn = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath("//button[contains(.,'Log In')]"))
+        test_FindAnElementButton_LogIn = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath("//button[contains(.,'Log In')]"))
         #print webElement.text
+        print 'test_FindAnElementButton_LogIn'
+        time.sleep(5)
 
 ##Find and Validate Link Elements
 
     def test_FindAnElementLink_Browsecourses(self):
         #Find Element by link text "Browse courses"
-        webElement_BrowseCourse = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Browse courses'))
+        test_FindAnElementLink_Browsecourses = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Browse courses'))
+        print 'test_FindAnElementLink_Browsecourses'
+        time.sleep(5)
 
     def test_FindAnElementLink_ForgotPassword(self):
         #Find Element by link text "Forgot Password?"
-        webElement_ForgotPassword = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Forgot Password?'))
+        test_FindAnElementLink_ForgotPassword = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Forgot Password?'))
+        print 'test_FindAnElementLink_ForgotPassword'
+        time.sleep(5)
 
     def test_FindAnElementLink_UserResearch(self):
         #Find Element by link text "User Research"
-        webElement_UserResearch = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('User Research'))
+        test_FindAnElementLink_UserResearch = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('User Research'))
+        print 'test_FindAnElementLink_UserResearch'
+        time.sleep(5)
 
     def test_FindAnElementLink_Help(self):
         #Find Element by link text "Help"
-        webElement_Help = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Help'))
+        test_FindAnElementLink_Help = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Help'))
+        print 'test_FindAnElementLink_Help'
+        time.sleep(5)
 
     def test_FindAnElementLink_Privacypolicy(self):
         #Find Element by link text "Privacy policy"
-        webElement_PrivacyPolicy = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Privacy policy'))
+        test_FindAnElementLink_Privacypolicy = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Privacy policy'))
+        print 'test_FindAnElementLink_Privacypolicy'
+        time.sleep(5)
 
     def test_FindAnElementLink_Termsofservice(self):
         ##Find Element by link text "Terms of service"
-        webElement_TermsofService = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Terms of service'))
+        test_FindAnElementLink_Termsofservice = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Terms of service'))
+        print 'test_FindAnElementLink_Termsofservice'
+        time.sleep(5)
 
     def test_FindAnElementLink_Facebook(self):
         #Find Element by link text "Facebook"
-        webElement_Facebook = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Facebook'))
+        test_FindAnElementLink_Facebook = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Facebook'))
+        print 'test_FindAnElementLink_Facebook'
+        time.sleep(5)
 
     def test_FindAnElementLink_Twitter(self):
         #Find Element by link text "Twitter"
-        webElement_Twitter = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Twitter'))
-
-
+        test_FindAnElementLink_Twitter = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements_by_link_text('Twitter'))
+        print 'test_FindAnElementLink_Twitter'
         time.sleep(5)
 
     def tearDown(self):
